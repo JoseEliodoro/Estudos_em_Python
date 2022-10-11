d3500d8a9a177b732c5ddad0ec667005970f5ea1
José <jose85751696@gmail.com>
2022-10-10 23:58:41 -0300

    Primeiro cammit da calculadora em python Criei uma classe chamada calculadora Tenho que organizar o método de widgets_frame01

--- exercicios curso/calculadora.py
+++ exercicios curso/calculadora.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from turtle import st, width
+from functools import partial
 
 class Calculadora:
     
@@ -6,7 +8,8 @@ class Calculadora:
         self.root = Tk()
         self.window()
         self.frames()
-        
+        self.widgets_frame01()
+        self.painel()
         self.root.mainloop()
     
     #Método para estilizar a janela
@@ -18,8 +21,83 @@ class Calculadora:
     #Método para criação dos frames
     def frames(self):
         #No frame01 está condido os botões
-        frame01 = Frame(self.root, bg='red', width=400, height=450)
-        frame01.grid(row=1)
-        frame02 = Frame(self.root, bg='blue', width=400, height=100)
-        frame02.grid(row=0)
+        self.frame01 = Frame(self.root, width=400, height=450)
+        self.frame01.grid(row=1)
+        
+        #No frame01 está condido os paineis de informação
+        self.frame02 = Frame(self.root, bg='blue', width=400, height=100)
+        self.frame02.grid(row=0)
+        
+    #Método para criar os widgets de botão
+    def widgets_frame01(self):
+        
+        self.buttons = list()
+        contx = 3
+        conty = 1
+        #Cria uma lista de botões de 1 a 9
+        for button in range(9, 0, -1):
+            
+            btn = Button(self.frame01, text= str(button), font='Arial 9 bold', bd=0.5)
+            btn.grid(column = contx, row=conty, pady= 1,  padx= 1)
+            
+            contx -= 1
+            if(contx == 0):
+                contx = 3
+                conty += 1
+                
+            self.buttons19(btn)
+            self.buttons.append(btn)
+            
+        self.btn_mult = Button(self.frame01, text= '*', font='Arial 9 bold', bd=0.5)
+        self.btn_sum = Button(self.frame01, text= '+', font='Arial 9 bold', bd=0.5)
+        self.btn_sub = Button(self.frame01, text= '-', font='Arial 9 bold', bd=0.5)
+        self.btn_divi = Button(self.frame01, text= '/', font='Arial 9 bold', bd=0.5)
+        self.btn0 = Button(self.frame01, text= '0', font='Arial 9 bold', bd=0.5)
+        self.btn_enter = Button(self.frame01, text= '=', font='Arial 9 bold', bd=0.5)
+        self.btn_ce = Button(self.frame01, text= 'C', font='Arial 9 bold', bd=0.5)
+        self.btn_pont = Button(self.frame01, text='.', font='Arial 9 bold', bd=0.5)
+            
+    
+        self.btn_ce.grid(column= 1, row=0, pady= 1,  padx= 1)
+        self.btn_divi.grid(column= 2, row=0, pady= 1,  padx= 1)
+        self.btn_mult.grid(column= 3, row=0, pady= 1,  padx= 1)
+        self.btn_sub.grid(column= 4, row=0, pady= 1,  padx= 1)
+        
+        self.buttons19(self.btn_ce)
+        self.buttons19(self.btn_divi)
+        self.buttons19(self.btn_mult)
+        self.buttons19(self.btn_sub)
+        
+        
+        self.btn_sum.grid(column= 4, row=1, rowspan=2, pady= 1,  padx= 1)
+        self.btn_enter.grid(column= 4, row=3, rowspan=2, pady= 1,  padx= 1)
+        
+        self.btn_enter['width'] = 11
+        self.btn_enter['height'] = 11
+        self.btn_sum['width'] = 11
+        self.btn_sum['height'] = 11
+        
+        self.btn0.grid(column= 1, row=4, columnspan=2, pady= 1,  padx= 1)
+        self.btn_pont.grid(column= 3, row=4, pady= 1,  padx= 1)
+        
+        self.buttons19(self.btn_pont)
+        
+        self.btn0['width'] = 23
+        self.btn0['height'] = 5
+    
+    #Método para mostra o painel
+    def painel(self):
+        result = Label(self.frame02, text='0', font='Arial 12 bold', width= 40, height=5)
+        result.grid()
+        
+    #Funçõe dos buttons
+    def function_buttons(self, a):
+        print(a)
+        
+    #Método para add estilo nos botões
+    def buttons19(self, button):
+        button['width'] = 11
+        button['height'] = 5
+        button['command'] = partial(self.function_buttons, button['text'])
+        
 Calculadora()
